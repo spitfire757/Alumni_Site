f1de859804beb463433ee2c79265e1ab4d6bc9b2
AJ Muir <andrew.muir.22@cnu.edu>
2024-01-25 10:33:12 -0500

    made changes to the first_contact page

---
+++ index/Reccomended_Alumni/first_contact.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        testing
+    </head>
+    <body>
+        <div>
+            <nav>
+                <table>
+                    <a href = "../help.php">Help</a>
+                    <a href = "../message.php">Message</a>
+                    <a href = "../forum.php">Forum</a>
+                    <a href = "../calendar.php">Calendar</a>
+                    <a href = "../profile.php">Profile</a>
+                </table>
+            </nav>
+        </div>
+        <div>
+            <img src = "images/christopher-newport-university-jerry-gammon.jpeg">
+        </div>
+        <div>
+            <h1>First Contact</h1>
+        </div>
+        <div>
+            <div>
+                <table>
+                    <tr>
+                        <p>Account Name<p>
+                        <input></input>
+                    </tr>
+                    <tr>
+                        <p>Account ID<p>
+                        <input></input>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <p>Enter the account name and ID of the who you would like to request to talk to</p>
+                        <button>Submit</button>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </body>
+    <?php
+        echo "hello world";
+
+    ?>
+</html>
