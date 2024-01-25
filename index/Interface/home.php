2cb6e9de454f3e82d3449d5ac92438d7bff5bff0
AJ Muir <andrew.muir.22@cnu.edu>
2024-01-25 10:48:55 -0500

    end of class 1 25 2024

---
+++ index/Interface/home.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        testing
+    </head>
+    <body>
+        <?php
+            echo "hello world";
+
+        ?>
+        <div>
+            <nav>
+                <table>
+                    <tr>
+                        <td>
+                            <img src = "images/christopher-newport-university-jerry-gammon.jpeg" href = "home.php">
+                        </td>
+                        <td>
+                            <a href = "../help.php">Help</a>
+                        </td>
+                        <td>
+                            <a href = "../message.php">Message</a>
+                        </td>
+                        <td>
+                            <a href = "../forum.php">Forum</a>
+                        </td>
+                        <td>
+                            <a href = "../calendar.php">Calendar</a>
+                        </td>
+                        <td>
+                            <a href = "../profile.php">Profile</a>
+                        </td>
+                    </tr>
+                </table>
+            </nav>
+        </div>
+        <div>
+            <img src = "images/christopher-newport-university-jerry-gammon.jpeg">
+        </div>
+        <div>
+            <h1>First Contact</h1>
+        </div>
+        <div>
+            <div>
+                <table>
+                    <tr>
+                        <p>Account Name<p>
+                        <input></input>
+                    </tr>
+                    <tr>
+                        <p>Account ID<p>
+                        <input></input>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <p>Enter the account name and ID of the who you would like to request to talk to</p>
+                        <button>Submit</button>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </body>
+    
+</html>
