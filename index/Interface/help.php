003b90c1c05f43c4c17e505dc10228347afe2b4f
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-01-29 14:13:46 -0500

    started help.php

---
+++ index/Interface/help.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+
+        ?>
+        <div>
+            <nav>
+                <table>
+                    <tr>
+                        <td>
+                            <a href = "../help.php">Help</a>
+                        </td>
+                        <td>
+                            <a href = "../message.php">Message</a>
+                        </td>
+                        <td>
+                            <a href = "../forum.php">Forum</a>
+                        </td>
+                        <td>
+                            <a href = "../calendar.php">Calendar</a>
+                        </td>
+                        <td>
+                            <a href = "../profile.php">Profile</a>
+                        </td>
+                    </tr>
+                </table>
+            </nav>
+        </div>
+        <div>
+            <h1>Tools to Lead a Life of Significance</h1>
+        </div>
+        <div>
+        <table>
+            <tr>
+                <td>
+                    <a href="https://cnu.edu/its/">CNU IT</a>
+                </td>
+                <td>
+                    <a href="https://cnu.edu">Account Help</a>
+                </td>
+            </tr>
+        <tr>
+            <td>
+                <a href="https://cnu.edu">CNU Help</a>
+            </td>
+            <td>
+                <a href="https://confluence.cnu.edu">Confluence</a>
+            </td>
+        </tr>
+        </table>
+        </div>
+    </body>
+</html>
+
