1559f680862a23c4e514d7d70f670b55caebcd66
Ubuntu <ubuntu@ip-172-31-90-74.ec2.internal>
2024-01-29 20:15:21 +0000

    making changes to the forum page

--- index/Interface/index.php
+++ index/Interface/index.php
@@ -1,80 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Technopoliz - We are Glad to Have You Here</title>
-    <style>
-        @font-face {
-            font-family: 'Press Start 2P';
-            src: url('press-start-2p.woff2') format('woff2'),
-                 url('press-start-2p.woff') format('woff');
-            /* Add more font file formats if available (e.g., .ttf, .otf) */
-            font-weight: normal;
-            font-style: normal;
-        }
-
-        body {
-            font-family: 'Roboto', sans-serif;
-            background-color: #0A2240; /* Dark background */
-            color: #c0c0c0; /* Light grey text */
-            text-align: center;
-            padding: 50px;
-            margin: 0;
-        }
-
-        h1 {
-            color: #E97451; /* Robotic blue */
-        }
-
-        p {
-            margin-bottom: 20px;
-            font-size: 18px;
-            line-height: 1.6;
-            font-family: 'Press Start 2P', sans-serif; /* Applying the Press Start 2P font */
-        }
-
-        .register-tab, .login-tab {
-            margin-top: 20px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .register-tab a, .login-tab a {
-            color: #4CBB17; /* Robotic orange */
-            text-decoration: none;
-            padding: 10px;
-            margin: 0 10px;
-            border: 1px solid #4CBB17; /* Robotic orange */
-            border-radius: 3px;
-            transition: background-color 0.3s;
-            font-weight: bold;
-        }
-
-        .register-tab a:hover, .login-tab a:hover {
-            background-color: #a0522d; /* Darker brown on hover */
-        }
-    </style>
-</head>
-<body>
-    <h1> Welcome o the CNU alumni system! </h1> 
-    <p>
-        Can you conquer Technopoliz? Technopoliz is an empire with flags that need to be captured!!
-        Can you solve the challenges to conquer the empire of Technopoliz, where you shall be the ruler?
-        We dare you to try!
-    </p>
-
-    <div class="register-tab">
-        <p>Don't have an account?</p>
-        <a href="register.php">Register</a>
-    </div>
-
-    <div class="login-tab">
-        <p>Already have an account?</p>
-	<a href="login.php" >Log In</a>
-    </div>
+<html>
+    <head>
+        testing
+    </head>
+    <body>
+        <?php
+            echo "hello world";
+        ?>
+        <div>
+            <nav>
+                <table>
+                    <tr>
+                        <td>
+                            <img src = "images/christopher-newport-university-jerry-gammon.jpeg" href = "home.php">
+                        </td>
+                        <td>
+                            <a href = "../help.php">Help</a>
+                        </td>
+                        <td>
+                            <a href = "../message.php">Message</a>
+                        </td>
+                        <td>
+                            <a href = "../forum.php">Forum</a>
+                        </td>
+                        <td>
+                            <a href = "../calendar.php">Calendar</a>
+                        </td>
+                        <td>
+                            <a href = "../profile.php">Profile</a>
+                        </td>
+                    </tr>
+                </table>
+            </nav>
+        </div>
+        <div>
+            <img src = "images/christopher-newport-university-jerry-gammon.jpeg">
+        </div>
+        <div>
+            <h1>First Contact</h1>
+        </div>
+        <div>
+            <div>
+                <table>
+                    <tr>
+                        <p>Account Name<p>
+                        <input></input>
+                    </tr>
+                    <tr>
+                        <p>Account ID<p>
+                        <input></input>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <p>Enter the account name and ID of the who you would like to request to talk to</p>
+                        <button>Submit</button>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </body>
     
-
-</body>
 </html>
