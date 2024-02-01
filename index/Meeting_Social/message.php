0f3cc98297e47bdb2cd66a8fe7baff2005778015
AJ Muir <andrew.muir.22@cnu.edu>
2024-02-01 10:29:54 -0500

    wrote pseudo code for message php

--- index/Meeting_Social/message.php
+++ index/Meeting_Social/message.php
@@ -1,9 +1,43 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <?php 
+        <?php
+            /*
+            !Connection to DB [No Schema Yet]
+
+            $dbhost = "localhost";
+            $dbuser = "mysql_user";
+            $dbpass = "r00tpassw0rd/";
+            $dbname = "DB";
+            
+            $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+            */
+
+            /*
+            Functionality
+
+            0. Database = Messages, Table = Chat/GroupChat 
+
+            1. Select table (chat) from database (messages) (maybe retrive from side panel from html/another php file?)
+
+            1. Select all rows from table (chat) that contains
+            - Timestamps (Primary Key) (DateTime)
+            - Sender/Profile (Foreign Key to pull information from another table)(VarChar(32))
+            - Message Text
+
+            2. Output Responses from Oldest to Newest (Top to Bottom)
+
+            3. If any user inputs thier response and hits send, the you insert the following into the table
+            - Timestamp of Send Button
+            - Sender/Profile information of Sender
+            - Message Text
+
+            ? Questions:
+            - Is there a way in PHP to dynamically update the page/php
+            since this is in an iframe
+            */
+
             echo "Hello World";
-            echo "Happy Birthday";
         ?>
     </body>
 </html>
