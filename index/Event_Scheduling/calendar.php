77d905e41158b974c54344dbc5c7060fbf93d2e2
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-02-01 09:51:26 -0500

    calender test

---
+++ index/Event_Scheduling/calendar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../style/global_style.css">
+    </head>
+    <body>
+        <?php
+            
+        ?>
+        <div>
+            <nav>
+                <table>
+                    <tr>
+                        <td>
+                            <a href = "../help.php">Help</a>
+                        </td>
+                        <td>
+                            <a href = "../message.php">Message</a>
+                        </td>
+                        <td>
+                            <a href = "../forum.php">Forum</a>
+                        </td>
+                        <td>
+                            <a href = "../calendar.php">Calendar</a>
+                        </td>
+                        <td>
+                            <a href = "../profile.php">Profile</a>
+                        </td>
+                    </tr>
+                </table>
+            </nav>
+        </div>
+    </body>
+<script type="text/javascript">
+$( "#datepicker" ).datepicker();
+</script>
+</html>
