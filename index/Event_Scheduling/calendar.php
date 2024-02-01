bf30d480e17b239b0c86b57ed836b9d6e7defcda
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-02-01 10:17:20 -0500

    calendar update

--- index/Event_Scheduling/calendar.php
+++ index/Event_Scheduling/calendar.php
@@ -4,6 +4,7 @@
         <link rel="stylesheet" href="../style/global_style.css">
     </head>
     <body>
+
         <div>
             <nav>
                 <table>
@@ -27,25 +28,22 @@
                 </table>
             </nav>
         </div>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>jQuery UI Datepicker - Default functionality</title>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-        <link rel="stylesheet" href="/resources/demos/style.css">
-        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-        <script>
-        $( function() {
-            $( "#datepicker" ).datepicker();
-        } );
-        </script>
-        </head>
-        <body>
- 
-        <p>Select a Date: <input type="text" id="eventDate" name="eventDate"></p>
-    <?php
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Datepicker - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script>
+    $( function() {
+        $( "#datepicker" ).datepicker();
+      } );
+    </script>
+</head>
+<body>
 
-    ?>
-    </body>
-    
+<p>Date: <input type="text" id="datepicker"></p>
+</body>
+
 </html>
