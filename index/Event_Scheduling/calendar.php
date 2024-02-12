1607b0ebcd7e089d31252dfd3b78a65317077f14
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-02-12 13:47:40 -0500

    create_event.php

--- index/Event_Scheduling/calendar.php
+++ index/Event_Scheduling/calendar.php
@@ -28,32 +28,10 @@
                 </table>
             </nav>
         </div>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>jQuery UI Datepicker - Default functionality</title>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-    <script>
-    $( function() {
-        $( "#datepicker" ).datepicker();
-      } );
-    </script>
-</head>
-<body>
-<h2> Create an Event</h2>
-<p>Select a Date: <input type="text" id="datepicker"></p>
-
-</body>
-<body>
-<form action="create_event.php">
-  <label for="appt">Select a time:</label>
-  <input type="time" id="appt" name="appt">
-  <input type="submit">
-</form>
-
-
-</body>
+<br>
+<a href = "create_event.php">Create Event</a>
+<h2> Upcoming Events</h2>
+<?php
+    
+?>
 </html>
---
+++ index/Event_Scheduling/create_event.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <link rel="stylesheet" href="../style/global_style.css">
+</head>
+<body>
+
+    <div>
+        <nav>
+            <table>
+                <tr>
+                    <td>
+                        <a href = "../Interface/help.php">Help</a>
+                    </td>
+                    <td>
+                        <a href = "../Meeting_Social/message.php">Message</a>
+                    </td>
+                    <td>
+                        <a href = "../Meeting_Social/forum.php">Forum</a>
+                    </td>
+                    <td>
+                        <a href = "calendar.php">Calendar</a>
+                    </td>
+                    <td>
+                        <a href = "../Authentication_Login/profile.php">Profile</a>
+                    </td>
+                </tr>
+            </table>
+        </nav>
+    </div>
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>jQuery UI Datepicker - Default functionality</title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script>
+    $( function() {
+        $( "#datepicker" ).datepicker();
+      } );
+    </script>
+</head>
+<body>
+<h2> Create an Event</h2>
+<p>Select a Date: <input type="text" id="datepicker"></p>
+
+</body>
+<body>
+<form action="calendar.php">
+  <label for="appt">Select a time:</label>
+  <input type="time" id="appt" name="appt">
+  <input type="submit">
+</form>
+
+
+</body>
+</htnl>
