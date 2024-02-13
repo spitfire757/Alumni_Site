6acec13558ebfd6a199854a35c0987c5d4ac3a5e
root <root@ip-172-31-90-74.ec2.internal>
2024-02-13 16:12:47 +0000

    Message can now connect to DB and binary for user settings added

--- index/Meeting_Social/Message2.php
+++ index/Meeting_Social/Message2.php
@@ -1,5 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+session_start();
+if (isset($_SESSION['username'])) {
+    $servername = "localhost";
+    $username = "mysql_user";
+    $password = "r00tpassw0rd/";
+    $dbname = "DB";
+
+    // Establish a connection to the database
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Current User : ",  $_SESSION['username'];
+    echo "<br> This is a test script for allowing the current unique user (stored in DB as userID) to stay signed in across webpages";
+    echo "<br> This page will only show up if a registered user is signed in";
+    echo "<br> Check connection : return a valid connection function:";
+} else {
+    echo "No user signed in, unable to connect to DB, sign in or contact DB admin";
+}
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -150,42 +173,4 @@
         }
     </script>
 
-<?php
-
-session_start();
-    if (isset($_SESSION['username'])) {
-    $servername = "localhost";
-    $username = "mysql_user";
-    $password = "r00tpassw0rd/";
-    $dbname = "DB";
-
-    // Establish a connection to the database
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Current User : ",  $_SESSION['username'];
-    echo "<br> This is a test script for allowing the current unique user (stored in DB as userID) 	to stay signed in across webpages";
-    echo "<br> This page will only show up if a registred user is signed in";
-    echo "<br> Check connection : return a valid connection function:";
-
-
-    }
-    // Between here and the else statement will be executed if a user is signed in
-    //
-    //
-    //
-    //
-    // Dont edit anything below for now
-    else{
-	    echo "No user signed in, unable to connect to DB, sign in or contact DB admin";
-    }
-    return $conn;
-
-?>
-
-
-</body>
 </html>
