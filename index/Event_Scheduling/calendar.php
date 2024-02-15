a09ba124dec591e589e05eb1c73e95f13eaace72
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-02-15 09:52:55 -0500

    calendar update

--- index/Event_Scheduling/calendar.php
+++ index/Event_Scheduling/calendar.php
@@ -35,21 +35,24 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
+$servername = "localhost";
+$username = "mysql_user";
+$password = "r00tpassw0rd/";
+$dbname = "DB";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $eventDate = $_POST["date"];
     $eventTime = $_POST["time"];
+    $eventTime += ":00 -5:00";
     $eventDetails = $_POST["bio"];
     $eventDateTimeOffset = $eventDate . " " . $eventTime;
-    $servername = "localhost";
-    $username = "mysql_user";
-    $password = "r00tpassw0rd/";
-    $dbname = "DB";
+    $eventDateTimeOffset = str_replace("/", "-", $eventDateTimeOffset);
+    $userID = 0;
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     // Insert the event into the database
-    $sql = "INSERT INTO Calendar (Date, Data) VALUES ('$eventDateTimeOffset', '$eventDetails')";
+    $sql = "INSERT INTO Calendar (User, Date, Data) VALUES ('$userID', '$eventDateTimeOffset', '$eventDetails')";
     if ($conn->query($sql) === TRUE) {
         echo "Event created successfully";
     } else {
@@ -57,11 +60,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     $conn->close();
 }
-// Display upcoming events
-$servername = "localhost";
-$username = "mysql_user";
-$password = "r00tpassw0rd/";
-$dbname = "DB";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -70,7 +69,7 @@ $sql = "SELECT * FROM Calendar";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "There is an Event " . $row['Date'] . ", " . $row['Data'] . "<br>";
+        echo "There is an Event " . $row['Date'] . ", " . $row['Data'] . " posted by userID: " . $row['User'] . "<br>";
     }
 } else {
     echo "No events currently scheduled";
