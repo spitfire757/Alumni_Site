64aefe0eca871d5490e5c745b28ab10daa556d32
root <root@ip-172-31-90-74.ec2.internal>
2024-02-16 01:05:55 +0000

    Working on messagging table

---
+++ index/Meeting_Social/message_table.php
@@ -0,0 +1,20 @@
+<
+
+
+<?php
+session_start();
+
+if (isset($_SESSION['username'])) {
+    $servername = "localhost";
+    $username = "mysql_user";
+    $password = "r00tpassw0rd/";
+    $dbname = "DB";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Current User :",  $_SESSION['username'];
+}
+?>
