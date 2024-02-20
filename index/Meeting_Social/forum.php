d8ebdbd71304a545f0a6194519a798a0869c5453
AJ Muir <andrew.muir.22@cnu.edu>
2024-02-20 00:49:15 -0500

    still debugging this

--- index/Meeting_Social/forum.php
+++ index/Meeting_Social/forum.php
@@ -24,6 +24,7 @@
                         $query = mysqli_query($conn,$sql);
 
                         $forum = (mysqli_fetch_array($query));
+                        echo $forum;
                     ?>
                     <h4><?php 
 
@@ -62,50 +63,6 @@
                 $result = $conn->query($sql);
                 
             ?>
-            <div>
-                <h3>
-                    <?php
-                        /*
-                        $dbhost = "localhost";
-                        $dbuser = "mysql_user";
-                        $dbpass = "r00tpassw0rd/";
-                        $dbname = "DB";
-                        
-                        $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-
-                        $post = $_POST["reply"];
-
-                        $sql = "SELECT * FROM Forum (table)";
-
-                        $replies = $conn->query($sql);
-
-                        while ($replies->num_rows>0){
-                            echo "hi";
-                        }
-
-                        $sql = "INSERT INTO `` VALUES ('".$year."'); ";
-
-                        $conn->query($sql);
-
-                        */
-
-                        /*
-                        $message = "dummy text, not in DB. There are four parts to a critical response paragraph:1) an argumentative topic sentence, 2) evidence in the form of quotations or paraphrases for the argument you are making, 3) interpretation of your evidence in relation to the argument, and 4) a strong concluding statement.";
-                        echo "<table>";
-                        $i = 10;
-                        while ($i>0) {
-                            echo "<tr style='text-align: right'>".$message."</tr><br><br>";
-                            $i --;
-                        }
-                        echo "</table>";
-                        
-                        $post = $_POST["reply"];
-
-                        echo "<p style='text-align: right'>".$post."</p><br>";
-                        */
-                    ?>
-                </h3>
-            </div>
         </div>
     </body>
 </html>
