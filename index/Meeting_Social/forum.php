c5bea7b05ccab12a9f9ffa0ce1c3fa10543f07d2
AJ Muir <andrew.muir.22@cnu.edu>
2024-02-20 10:07:00 -0500

    lord have mercy

--- index/Meeting_Social/forum.php
+++ index/Meeting_Social/forum.php
@@ -51,8 +51,9 @@
             <?php
 
                 $append_response = '|new|' . $_POST["reply"];
+                echo $append_response;
                 #SQL works in terms of updating code
-                $sql = "UPDATE Forum SET replies = replies + ('".$append_response."') WHERE postID = ('".$forum["postID"]."');";
+                $sql = "UPDATE Forum SET replies = (replies + ".$append_response.") WHERE postID = (".$forum["postID"].");";
                 $result = $conn->query($sql);
                 
             ?>
