d7e92aa1e755e545beaaf1789974c1f7e23d9467
AJ Muir <andrew.muir.22@cnu.edu>
2024-02-19 23:19:14 -0500

    didn't add new file

---
+++ index/Meeting_Social/forum-view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../style/global_style.css">
+    </head>
+    <body>
+        <div>
+            <?php 
+                include 'forum.php';
+                echo $forum[3];
+            ?>
+        </div>
+    </body>
+<html>
