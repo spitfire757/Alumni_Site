c45177aeede8b3c80b118da29f36763dc787c5eb
AJ Muir <andrew.muir.22@cnu.edu>
2024-03-12 10:42:36 -0400

    dsfsd

--- index/Meeting_Social/forum.php
+++ index/Meeting_Social/forum.php
@@ -18,42 +18,24 @@
 
                     $conn = new mysqli($servername, $username, $password, $dbname);
 
-                    $sql = "SELECT Title FROM Forum";
-                    $query = mysqli_query($conn, $sql);
-                    $forum = mysqli_fetch_assoc($query);
+                    $sql = "SELECT ForumID, Title FROM Forum";
+                    $query = $conn->query($sql);
+                    
+                    
+                    while ($row = $query->fetch_assoc()){
+                        echo $row["Title"];
+                    }
 
-                    echo $forum;
+                    
                 ?>
             <!-- Form for the forum -->
             </div>
             <form action = "forum.php" method = "post">
-                    <!-- This should load the content when selected -->
-                    <select name = "thread">
-                        <option value="start"><?php echo isset($_SESSION["thread"]) ? $_SESSION["thread"] : ""; ?></option>
-                        <option value="default">Select a Thread</option>
-                        <option value="<?php echo $forum["ForumID"]; ?>"><?php echo $forum["Title"]; ?></option>
-                        <option value="testing">Testing</option>
-                    </select>
-                    <button type = "submit" name = "refresh" formaction="forum.php">Go</button><br>
-     
-                    <h2><?php echo $forum["Title"];?></h2>
-                    <h3><?php echo $forum["userID"];?></h3>
-                    <p><?php echo $forum["Description"];?></p>
-                    <input type="text" name="userID" placeholder="userID" maxlength="64"><br>
-                    <textarea id="response" name="response" placeholder="Insert Your Response Here" rows="4" cols="50" maxlength="255"></textarea><br>                  
-                    <button type = "submit" name = "submit" formaction="forum.php" required>Reply</button><br>      
+                    <!-- This should load the content when selected -->     
             </form>
             <?php
                 if (isset($_POST['submit'])) {
 
-                    # Get the elements needed for a new response
-                    $userID = $_POST['userID'];
-                    $response = $_POST['response'];
-                    $dateTime = date("m-d-Y H:i:s");
-                    $responseID = substr(hash('sha256',$response),0,16);
-                    $forumID = $forum["ForumID"];
-
-                    echo "userID: ".$userID."<br>datetime: ".$dateTime."<br>response: ".$response."<br>forumID: ".$forumID."<br> responseID: ".$responseID."<br>";
                     
                     /*
                     # Add Response to Response Table
