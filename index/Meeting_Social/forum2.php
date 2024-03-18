e3419a631dbb2ee91f0329ad97f4816c51811ace
AJ Muir <andrew.muir.22@cnu.edu>
2024-03-18 16:22:40 -0400

    redoing forum

--- index/Meeting_Social/forum2.php
+++ index/Meeting_Social/forum2.php
@@ -12,18 +12,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 $query = "SELECT * FROM Forum";
 $result = mysqli_query($conn, $query);
 
-// Display forum titles and descriptions
-while ($row = mysqli_fetch_assoc($result)) {
-    // Link to response.php with forum ID as parameter
-    echo "<a href='response2.php?name={$row['ForumID']}'>{$row['Title']}</a><br>";
-    echo "<p>{$row['Description']}</p>";
-}
-
 if(isset($_GET['name'])) {
     // If a forum is selected, save its information in session
     $forumID = $_GET['name'];
     $_SESSION["ForumID"] = $forumID;
     // Redirect to response2.php
+}
+
+// Display forum titles and descriptions
+while ($row = mysqli_fetch_assoc($result)) {
+    // Link to response.php with forum ID as parameter
+    echo "<a href='response2.php?name={$row['ForumID']}'>{$row['Title']}</a><br>";
+    echo "<p>{$row['Description']}</p>";
 }
 
 // Close connection
