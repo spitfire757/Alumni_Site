87095fff272d40dacc245029bb96bfb3ee1b2db9
AJ Muir <andrew.muir.22@cnu.edu>
2024-03-18 15:07:45 -0400

    redoing forum

--- index/Meeting_Social/forum2.php
+++ index/Meeting_Social/forum2.php
@@ -14,10 +14,10 @@ $result = mysqli_query($conn, $query);
 
 // Display forum titles and descriptions
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<h2>{$row['title']}</h2>";
-    echo "<p>{$row['description']}</p>";
+    echo "<h2>{$row['Title']}</h2>";
+    echo "<p>{$row['Description']}</p>";
     // Link to response.php with forum ID as parameter
-    echo "<a href='response2.php?forumID={$row['forumID']}'>Respond</a><br><br>";
+    echo "<a href='response2.php?forumID={$row['ForumID']}'>Respond Here</a><br><br>";
 }
 
 // Close connection
