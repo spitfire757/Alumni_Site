9c13c6df0389021149b33910cbf34dd232a57da7
root <root@ip-172-31-90-74.ec2.internal>
2024-03-22 03:23:53 +0000

    Update pre server upgrade

--- index/Meeting_Social/profile_content.php
+++ index/Meeting_Social/profile_content.php
@@ -19,27 +19,28 @@ if (isset($_SESSION['username'])) {
         $major = $_POST['major'];
         $minor = $_POST['minor'];
         $experience = $_POST['experience'];
+        $security = $_POST['securityToggle']; // Added
 
         // Validate the character limit for Experience
         if (strlen($experience) > 500) {
             echo "Error: Experience must not exceed 500 characters.";
         } else {
             // Update the user information in the database
-            $updateSql = "UPDATE User SET Major=?, Minor=?, Experience=? WHERE email=?";
+            $updateSql = "UPDATE User SET Major=?, Minor=?, Experience=?, security=? WHERE email=?";
             $updateStmt = $conn->prepare($updateSql);
 
             if ($updateStmt === false) {
                 die("Error in preparing the update statement: " . $conn->error);
             }
 
-            $updateStmt->bind_param("ssss", $major, $minor, $experience, $currentUser);
+            $updateStmt->bind_param("sssss", $major, $minor, $experience, $security, $currentUser); // Modified
             $updateStmt->execute();
             $updateStmt->close();
         }
     }
 
     // Retrieve UserID and other fields based on the email (username)
-    $sql = "SELECT UserID, email, Fname, LName, Major, Minor, Experience FROM User WHERE email = ?";
+    $sql = "SELECT UserID, email, Fname, LName, Major, Minor, Experience, security FROM User WHERE email = ?";
     $stmt = $conn->prepare($sql);
 
     if ($stmt === false) {
@@ -58,30 +59,34 @@ if (isset($_SESSION['username'])) {
         $LName = $row['LName'];
         $major = $row['Major'];
         $minor = $row['Minor'];
-	$security  = $row['security'];
-	$experience = $row['Experience'];
-	$pset_off = "Your information Such as Name, Major, Minor, and Experience are hidden from searches";
-        $pset_on  = "Your information such as Name, Major, Minor, and Experience will show in searches";	
-	#if ($security is_null()) {
-	#	echo $pset_off;	
-	#}
-	# Need to figure out security setting options for search
-	# And Profile Type 
-	#
+        $security = $row['security'];
+        $experience = $row['Experience'];
+        $pset_off = "Your information Such as Name, Major, Minor, and Experience are hidden from searches";
+        $pset_on  = "Your information such as Name, Major, Minor, and Experience will show in searches";
+
         echo "UserID for $currentUser: $userID <br>";
         echo "Email: $email <br>";
         echo "Name: $FName $LName <br>";
         echo "Major: $major <br>";
         echo "Minor: $minor <br>";
         echo "Experience: $experience <br>";
-	echo "Privacy Settings: $security";
+        echo "Privacy Settings: ";
+        echo ($security == 1) ? $pset_on : $pset_off; // Display privacy settings based on the security value
 
         // Display the form for updating information
         echo "<form method='post' action=''>";
-	echo "<br>";
+        echo "<br>";
         echo "Major: <input type='text' name='major' value='$major'><br>";
         echo "Minor: <input type='text' name='minor' value='$minor'><br>";
         echo "Experience: <textarea name='experience'>$experience</textarea><br>";
+        
+        // Security Settings Dropdown
+        echo "<label for='securityToggle'>Security Setting:</label>";
+        echo "<select id='securityToggle' name='securityToggle' required>";
+        echo "<option value='0'" . ($security == 0 ? ' selected' : '') . ">On</option>";
+        echo "<option value='1'" . ($security == 1 ? ' selected' : '') . ">Off</option>";
+        echo "</select><br>";
+
         echo "<input type='submit' value='Update'>";
         echo "</form>";
     } else {
