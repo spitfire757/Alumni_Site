0a69aa4c6f44cbd628fae8312a64ec92112f1e8b
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-04-11 10:12:48 -0400

    test

--- index/Event_Scheduling/create_event.php
+++ index/Event_Scheduling/create_event.php
@@ -3,58 +3,12 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../style/global_style.css">
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-            text-align: center;
-        }
-
-        .tabs {
-            display: flex;
-            background-color: #305A8C; /* Updated color code, feel free to change it*/
-            color: white;
-        }
-
-        .tab {
-            flex: 1;
-            padding: 15px;
-            text-align: center;
-            cursor: pointer;
-        }
-
-        .tab:hover {
-            background-color: #2A4C7D;
-        }
-
-        .content {
-            padding: 20px;
-        }
-
-        .buttons {
-            margin-top: 20px;
-            justify-content: center;
-        }
-
-        .button {
-            padding: 10px 20px;
-            background-color: #305A8C;
-            color: white;
-            border: none;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-        }
-
-        .button:hover {
-            background-color: #2A4C7D;
-        }
-
-        /* Style for input and textarea */
+        
 input[type="text"], textarea {
     width: 50%;
-height: 40px;
+    height: 40px;
     padding: 10px;
     margin: 5px 0;
     box-sizing: border-box;
@@ -74,7 +28,7 @@ input[type="date"] {
     resize: none;
 }
     </style>
-    <link rel="stylesheet" href="../style/global_style.css">
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Calendar</title>
@@ -97,6 +51,7 @@ input[type="date"] {
         <input type="text" id="bio" name="bio" required><br>
         <input type="submit" value="Create">
     </form>
+    <p style="text-align:center"><a href="calendar.php">Cancel Event Creation</a></p>
 </body>
 </html>
 
