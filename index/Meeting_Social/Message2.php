8991dff43843df8ed21f95456da4c9315c3a2342
AJ Muir <andrew.muir.22@cnu.edu>
2024-04-11 10:27:20 -0400

    1234567

--- index/Meeting_Social/Message2.php
+++ index/Meeting_Social/Message2.php
@@ -251,13 +251,17 @@ else {
     </table>
 </div>
 
-
+    <!--
     <div id="helppage" class="content" style="display: none;">
         <h2>Captain's Dock - Help Page Tab</h2>
-<iframe src="help.php" width="100%" height="800px" frameborder="0"></iframe><br>
+    <iframe src="help.php" width="100%" height="800px" frameborder="0"></iframe><br>
     </div>
+                -->
 
-
+    <div id="helppage" class="content" style="display: none; text-align: center;">
+        <h2 style="font-family: Trajan Pro, sans-serif;">Captain's Dock - Help Page Tab</h2>
+        <iframe src="help.php" width="100%" height="800px" frameborder="0"></iframe><br>
+    </div>
 
     <div id="messages" class="content" style="display: none;">
         <h2>Captain's Dock - Messages Tab</h2>
--- index/Meeting_Social/help.php
+++ index/Meeting_Social/help.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html>
     <head>
@@ -40,4 +41,74 @@
         </div>
     </body>
 </html>
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../style/global_style.css">
+    <style>
+        /* Button styles */
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 1rem;
+            font-weight: bold;
+            text-decoration: none;
+            color: #fff;
+            background-color: #007bff;
+            border: 2px solid transparent;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: all 0.3s ease;
+        }
+
+        .btn:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+
+        /* Table styles */
+        table {
+            margin: auto;
+            width: 80%;
+        }
+
+        td {
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body style="text-align: center; font-family: Trajan Pro, sans-serif;">
+    <div>
+        <h2>Tools to Lead a Life of Significance</h2>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td>
+                    <a href="https://cnu.edu/its/" target="_blank" class="btn">CNU IT Services</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://my.cnu.edu" target="_blank" class="btn">My CNU</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://cnu.edu/help/" target="_blank" class="btn">CNU Help Page</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://confluence.cnu.edu" target="_blank" class="btn">Confluence</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
+
 
