da2d83d32f2c84ca1426f2f0e0a607b9934a70ac
Brayden Hagemeier <brayden.hagemeier.21@cnu.edu>
2024-04-11 09:57:54 -0400

    cleaning up calendar

--- index/Meeting_Social/help.php
+++ index/Meeting_Social/help.php
@@ -14,21 +14,25 @@
         <table>
             <tr>
                 <td>
-                    <a href="https://cnu.edu/its/" target="_blank">CNU IT</a>
+                    echo "Having issues with your laptop or need to contact the help desk?<br>";
+                    <a href="https://cnu.edu/its/" target="_blank">CNU IT Services</a>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <a href="https://cnu.edu" target="_blank">Account Help</a>
+                    echo "My CNU has many student resources such as access to CNU live and the housing portal<br>";
+                    <a href="https://my.cnu.edu" target="_blank">My CNU</a>
                 </td>
             </tr>
             <tr>
                 <td>
-                <a href="https://cnu.edu/help/" target="_blank">CNU Help</a>
+            echo "CNU's help page has places for you to submit housing/facilites maintenance requests, change your CNU password, and much more<br>";
+                <a href="https://cnu.edu/help/" target="_blank">CNU Help Page</a>
                 </td>
             </tr>
             <tr>
                 <td>
+                echo "Confluence has step by step guides for several tech issues students may have like installing PrintAnywhere or Microsoft Office<br>";
                 <a href="https://confluence.cnu.edu" target="_blank">Confluence</a>
             </td>
             </tr>
